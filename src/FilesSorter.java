97c74bdb34ac5643bd9cd042181be6c8671fc76b
Aram Vanyan <aram.vanyan.a@gmail.com>
2020-01-19 14:07:57 +0400

    change the name of packages

--- src/FilesSorter.java
+++ src/FilesSorter.java
@@ -4,6 +4,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Objects;
+/*
+* Files sorter written in java. Pass the path to root directory and the file itself,
+* then all files in root directory will be sorted into appropriate packages
+* */
 
 class FilesSorter {
     private static void moveFile(String src, String dest ) {
@@ -19,21 +23,20 @@ class FilesSorter {
             String filename= file.getName();
             String fileNameArray[] = filename.split("\\.");
             File directoryForSpecificFiles = new File((String.valueOf(pathToRootDirectory)
-                                                       + "\\" + fileNameArray[fileNameArray.length-1]).toUpperCase()
-                                                       + "files");
+                                                       + "\\" + fileNameArray[fileNameArray.length-1])
+                                                       + "_files");
             directoryForSpecificFiles.mkdir();
             moveFile(file.getAbsolutePath(),directoryForSpecificFiles.getAbsolutePath() + "\\" + filename);
             return;
         }
+
         File[] files = file.listFiles();
 
-            for (int i = 0; i < files.length; i++) {
-                sortFilesIntoAppropriatePackages(files[i], pathToRootDirectory);
-            }
-            if (Objects.requireNonNull(file.listFiles()).length == 0) {
+        for (int i = 0; i < files.length; i++) {
+            sortFilesIntoAppropriatePackages(files[i], pathToRootDirectory);
+        }
+        if (Objects.requireNonNull(file.listFiles()).length == 0) {
                 file.delete();
-            }
-
-
+        }
     }
 }
